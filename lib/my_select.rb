2890f7269a6748a510f404dc77150c1e10d6496b
Scott Ungchusri <animoogle504@gmail.com>
2018-07-10 16:53:38 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,18 +2,18 @@ def my_select(collection)
  # your code here!
  if block_given?
    i = 0 #iterator
-   count = 0 # secondary iterator for temp[]
+   #count = 0 # secondary iterator for temp[]
    temp = []
    
    while i<collection.length 
     bool = yield(collection[i])
     if bool
-      temp[count] =  collection[i]
+      temp[i] =  collection[i]
       count += 1 
     end
     i+=1
    end
-   temp
+   temp.compact!
  else
    #nothing
  end
