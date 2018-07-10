8124b1b2c9e6dd90d6f1f3f5896bc158f9cadf0c
Scott Ungchusri <animoogle504@gmail.com>
2018-07-10 16:58:12 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,20 @@
 def my_select(collection)
  # your code here!
+ if block_given?
+   i = 0                    #iterator for the passed in array 
+   count = 0                # secondary iterator for temp[]
+   temp = []                #return array
+   
+   while i<collection.length 
+    bool = yield(collection[i])           #test true/false
+    if bool                               #if whatever method shows true,
+      temp[count] =  collection[i]        #assign that value to our array temp at count
+      count += 1                          #iterate count otherwise just iterate i to keep going
+    end
+    i+=1
+   end
+   temp                                   #returns temp[]
+ else
+   #nothing
+ end
 end
